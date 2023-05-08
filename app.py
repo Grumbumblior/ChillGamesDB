e05e1ef79bd1533e726f75ab15c39f3377399afd
Grumbumblior <drake.misfeldt@gmail.com>
2023-05-08 17:02:21 -0500

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'longRandomString'
 
 def get_db_connection():
   conn = sqlite3.connect('database.db')
@@ -15,3 +16,7 @@ def index():
   games = conn.execute('SELECT * FROM gametitle').fetchall()
   conn.close()
   return render_template('index.html', games=games)
+
+@app.route('/addgame/', methods=('GET', 'POST'))
+def addgame():
+    return render_template('addgame.html')
