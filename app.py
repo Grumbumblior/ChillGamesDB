b57411cd7d35014df04a7433110ddf826d439c1f
Grumbumblior <drake.misfeldt@gmail.com>
2023-05-09 00:30:39 -0500

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'longRandomString'
@@ -9,6 +9,14 @@ def get_db_connection():
   conn.row_factory = sqlite3.Row
   return conn
 
+def get_game(game_id):
+    conn = get_db_connection()
+    game = conn.execute('SELECT * FROM gametitle WHERE game_id = ?',
+                        (game_id,)).fetchone()
+    conn.close()
+    if game is None:
+        abort(404)
+    return game
 
 @app.route('/')
 def index():
@@ -19,4 +27,71 @@ def index():
 
 @app.route('/addgame/', methods=('GET', 'POST'))
 def addgame():
-    return render_template('addgame.html')
+  if request.method == 'POST':
+    game_id = request.form['game_id']
+    title = request.form['title']
+    genre = request.form['genre']
+    description = request.form['description']
+    dev_id = request.form['dev_id']
+    price = request.form['price']
+
+    if not title:
+      flash('Title is required!')
+    elif not genre:
+      flash('Genre is required')
+    elif not price:
+      flash('Price is required')
+    elif not description:
+      flash('Description is required!')
+    else:
+      conn = get_db_connection()
+      #Here I try to auto increment game id and dev id, 
+      # but that won't work because you can't USE values from the table without flask alchemy.
+      #Right now a blank game or dev id will crash the website, but i'm leaving this as a reminder.
+      if not game_id:
+        game_id = conn.execute('SELECT max(game_id) FROM gametitle').fetchone()
+        game_id = game_id + 1
+      if not game_id:
+        dev_id = conn.execute('SELECT max(dev_id) FROM gametitle').fetchone()
+        dev_id = dev_id + 1
+      
+      #this works fine, but the price is displayed strangely
+      conn.execute('INSERT INTO gametitle (game_id, title, genre, description, dev_id, price) VALUES (?,?,?,?,?,?)',
+                    (game_id, title, genre, description, dev_id, price))
+      conn.commit()
+      conn.close()
+      return redirect(url_for('index'))
+    
+  return render_template('addgame.html')
+
+@app.route('/<int:game_id>/edit/', methods=('GET', 'POST'))
+def edit(game_id):
+    game = get_game(game_id)
+
+    if request.method == 'POST':
+        game_id = request.form['game_id']
+        title = request.form['title']
+        genre = request.form['genre']
+        description = request.form['description']
+        dev_id = request.form['dev_id']
+        price = request.form['price']
+
+        if not title:
+          flash('Title is required!')
+        elif not genre:
+          flash('Genre is required')
+        elif not price:
+          flash('Price is required')
+        elif not description:
+          flash('Description is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE gametitle SET game_id = ?, title = ?, genre = ?, description = ?, dev_id = ?, price = ?'
+                         ' WHERE game_id = ?',
+                         (game_id, title, genre, description, dev_id, price, game_id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', game=game)
