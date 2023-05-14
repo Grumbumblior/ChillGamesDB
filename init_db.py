7a9dfb595ef74efaa266c36f20948b0ae94e2a94
Elizabeth Sloan <esloan94@gmail.com>
2023-05-14 13:08:27 -0500

    make it pretty

--- init_db.py
+++ init_db.py
@@ -14,10 +14,22 @@ cur.execute(
    1, 7.99))
 
 cur.execute(
+  "INSERT INTO test1 (game_id, title, description) VALUES (?,?,?)",
+  (1, 'A Short Hike',
+   'Hike, climb, and soar through the peaceful mountainside landscapes of Hawk Peak Provincial Park as you make your way to the summit.'))
+
+
+cur.execute(
   "INSERT INTO gametitle (game_id, title, genre, description, dev_id, price) VALUES (?,?,?,?,?,?)",
   (2, 'Stardew Valley', 'Farming Sim',
    "You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?",
    2, 14.99))
+
+cur.execute(
+  "INSERT INTO test2 (game_id, title, description) VALUES (?,?,?)",
+  (2, 'Stardew Valley',
+   "You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?",
+  ))
 
 cur.execute(
   "INSERT INTO gametitle (game_id, title, genre, description, dev_id, price) VALUES (?,?,?,?,?,?)",
@@ -26,10 +38,22 @@ cur.execute(
    3, 59.99))
 
 cur.execute(
+  "INSERT INTO test3 (game_id, title, description) VALUES (?,?,?)",
+  (3, 'Animal Crossing',
+   'Escape to a deserted island and create your own paradise as you explore, create, and customize in the Animal Crossing: New Horizons game.'
+   ))
+
+cur.execute(
   "INSERT INTO gametitle (game_id, title, genre, description, dev_id, price) VALUES (?,?,?,?,?,?)",
   (4, 'Legend of Zelda: Breath of the Wild', 'Exploration',
    "Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series.",
    4, 59.99))
+
+cur.execute(
+  "INSERT INTO final (game_id, title, description) VALUES (?,?,?)",
+  (4, 'Legend of Zelda: Breath of the Wild',
+   "Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series."))
+
 
 cur.execute(
   "INSERT INTO gamedeveloper (dev_id, dev_name) VALUES (?,?)",
